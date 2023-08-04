53d86a265e8b324acd1d2bba646230e58129dd07
sudo-Tiz <tiz@computiz>
2023-08-04 04:45:36 +0200

    F2 to switch theme

--- config.h
+++ config.h
@@ -283,6 +283,7 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
+	{ MODKEY,               XK_F2,           invert,         { } },
 };
 
 /*
